5f91592fe4ca9866fb68b14fd83157a27cf0779d
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-03-29 18:20:09 +0100

    repeater

--- src/components/repeater.js
+++ src/components/repeater.js
@@ -5,18 +5,10 @@ class Repeater extends React.Component {
         super(props);
         this.state = {
             length: 20
-        }
-        
-        //this._onScroll = this._onScroll.bind(this)
+        }        
     }
 
     componentDidMount(){
-        //const {length} = this.state
-        //const items = Array.from(new Array(length),(val,index)=>index);
-        
-        // document.addEventListener("scroll", this._onScroll)
-        // this._onScroll()
-
         this._tick()
     }
 
@@ -24,15 +16,9 @@ class Repeater extends React.Component {
         const scroller = this.refs.scroller;
         const {length} = this.state
         setInterval(() => {
-            //let topPos = scroller.offsetTop + 10;
             //console.log(scroller.scrollTop + scroller.offsetHeight, scroller.scrollHeight)
             scroller.scrollTop += 1;
-			// $(".scrollSide").animate({scrollTop:"+=10px"}, 100, function () {  
-			// 	if(this.scrollTop + this.offsetHeight >= this.scrollHeight) {
-			// 		var clone = $('.scrollSide').children('figure').eq(0).clone();
-			// 		$('.scrollSide').append(clone);
-			// 	}
-            // });
+
             if(scroller.scrollTop + scroller.offsetHeight >= scroller.scrollHeight) {
                 this.setState({
                     length: length + 20
@@ -47,7 +33,11 @@ class Repeater extends React.Component {
 
         const arr = Array.from(new Array(length),(val,index)=>index);
         const items = arr.map(index => (
-                <div key={index}>{title}</div>
+                <div 
+                key={index}
+                className='item blink' style={{
+                    animationDelay: index+"00ms"
+                }}>{title}</div>
         ))
         return items
     }
