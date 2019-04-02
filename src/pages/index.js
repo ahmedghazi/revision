b85531a246eed37e8956b7150abface2322940cf
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-04-02 16:38:05 +0200

    majs

--- src/pages/index.js
+++ src/pages/index.js
@@ -96,7 +96,7 @@ class Index extends React.Component {
                                         <a 
                                         href={li.url} 
                                         target="_blank"
-                                        rel="noopener noreferrer">↗ {li.label}</a>
+                                        rel="noopener noreferrer">{li.label}</a>
                                     </li>
                                 ))}
                             </ul>
@@ -108,13 +108,13 @@ class Index extends React.Component {
                                         <a 
                                             href="https://www.notion.so/revisionio/LEGAL-DISCLOSURE-6a2e89e5b0ff494ca40ef250871530de?fbclid=IwAR0Vt9QjfSkqW0wlpOupuupHNRLXEOy9krvFG2ump32uU_ZlWB-rXZhVJOE"
                                             target="_blank"
-                                            rel="noopener noreferrer">LEGAL</a>
+                                            rel="noopener noreferrer">Legal</a>
                                     </li>
                                     <li>
                                         <a 
                                             href="https://www.notion.so/revisionio/PRIVACY-POLICY-b24ee8d7bd4e477e9750a67833fcf9a1?fbclid=IwAR1jXGSLXQnrEaT5_aoQP_Lo9DY2HY8q9Mk8i-UvXpEZn4xJyKh8j3btRXE"
                                             target="_blank"
-                                            rel="noopener noreferrer">Privacy policy</a>
+                                            rel="noopener noreferrer">Privacy</a>
                                     </li>
                                 </ul>
                                 <div className="copy">
