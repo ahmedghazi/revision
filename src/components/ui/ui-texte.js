0ec116cacd390a8eb79a79293a78efd600367a7e
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-05-28 09:53:17 +0200

    ios

--- src/components/ui/ui-texte.js
+++ src/components/ui/ui-texte.js
@@ -4,14 +4,42 @@ import People from "./people"
 import Truncate from 'react-clamp'
 
 class Texte extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numLines: 6
+        }        
+    }
+
+    componentDidMount(){
+        const {
+            data
+        } = this.props
+        console.log(data.size)
+
+        let numLines = 0
+        if('ontouchstart' in window){
+            numLines = data.size === '1/4' ? 5 : 9
+        }else{
+            numLines = data.size === '1/4' ? 6 : 9
+        }
+
+        this.setState({
+            numLines: numLines
+        })
+    }
     render() {
+        const {
+            numLines
+        } = this.state
+        
         const {
             data
         } = this.props
 //console.log(data)
-        const numLines = data.size === '1/4' ? 6 : 9
+        //const numLines = data.size === '1/4' ? 6 : 9
         return (
-            <div className="ui-texte">
+            <div className="ui-texte" data-lines={numLines+" "+data.size}>
                 {data.texte !== "" &&
                     // <Truncate className="texte" 
                     // lines={numLines}
