f2179f79613325a5f519bf96562935db08bb003f
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-06-25 17:52:02 +0200

    404 seo

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "gatsby"
+import Helmet from "react-helmet";
+import favicon from '../images/favicon.png';
 
 require('../styles/index.scss')
 
@@ -9,8 +11,29 @@ class Page404 extends Component {
             options,
             articles
           } = this.props.data
+          console.log(options)
         return (
             <div className="page-404">
+                 <Helmet>
+                    <meta charSet="utf-8" />
+                    <link rel='shortcut icon' type="image/png" href={favicon} />
+
+                    <title>{options.title}</title>
+                    <meta name="description" content={options.description.childMarkdownRemark.rawMarkdownBody} />
+                    <meta property="og:url" content="" />
+                    <meta property="og:title" content={options.title} />
+                    <meta property="og:description" content={options.description.childMarkdownRemark.rawMarkdownBody} />
+                    <meta property="og:image" content={options.image.file.url} />
+
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:site" content="@Revision_HQ" />
+                    <meta name="twitter:creator" content="@Revision_HQ" />
+                    <meta property="og:url" content="" />
+                    <meta property="og:title" content={options.title} />
+                    <meta property="og:description" content={options.description.childMarkdownRemark.rawMarkdownBody} />
+                    <meta property="og:image" content={options.image.file.url} />
+              </Helmet>
+
                 <header>
                     <div className="sitename">
                     <Link to="/">{options.title}</Link>
@@ -40,8 +63,8 @@ class Page404 extends Component {
 
 export default Page404;
 
-export const pageQuery = graphql `
-  query Page404 {
+export const query = graphql `
+  query {
     options: contentfulOptions{
       title
       description{
